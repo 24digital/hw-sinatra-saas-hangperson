c64b09cfa4ed95d405c104decd96176f41b16355
a24digital <24digital@gmail.com>
2015-09-27 20:47:12 +0000

    setup gemfile

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,10 +7,61 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  attr_accessor :word, :guesses, :wrong_guesses
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
+  
+  def guess(letter)
+    if (letter.nil? or !(letter =~ /^[[:alpha:]]$/))
+			raise ArgumentError	
+	 end
+    letter.downcase!
+    if @word.include? letter
+      if !@guesses.include? letter
+      
+        @guesses += letter
+        return true
+      else
+        return false
+      end
+    else
+      if !@wrong_guesses.include? letter.upcase and !@wrong_guesses.include?(letter.downcase)
+       @wrong_guesses += letter
+      else
+        return false
+      end
+    end
+  end
+  
+  def word_with_guesses
+    space = ''
+    word.each_char do|i|
+			if @guesses.include? i
+				space << i
+			else
+				space << '-'
+			end
+		end
+		return space
+  end
+  
+  def check_win_or_lose
+    if wrong_guesses.length >= 7
+      return :lose
+    end
+    
+   word.each_char do|i|
+			if !@guesses.include? i
+				return :play
+			end
+		end
+		return :win
+		
+  end
+ 
 
   def self.get_random_word
     require 'uri'
---
+++ lib/test.rb
@@ -0,0 +1,3 @@
+require 'Hangerperson'
+@name = hangeperson::HangerpersonGame.new('a')
+puts @name.guess
